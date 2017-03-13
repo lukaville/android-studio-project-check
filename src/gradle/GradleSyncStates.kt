3b5c9265bb65add5d2fdbb777e01b632aa5b1860
nlchameev <nlchameev@avito.ru>
2017-03-13 14:30:09 +0300

    Update Android Studio version

--- src/gradle/GradleSyncStates.kt
+++ src/gradle/GradleSyncStates.kt
@@ -1,15 +1,20 @@
 package gradle
 
-import com.android.tools.idea.gradle.GradleSyncState
-import com.android.tools.idea.gradle.project.GradleProjectImporter
-import com.android.tools.idea.gradle.project.GradleSyncListener
+import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker
+import com.android.tools.idea.gradle.project.sync.GradleSyncListener
+import com.android.tools.idea.gradle.project.sync.GradleSyncState
 import com.intellij.openapi.project.Project
 
 fun Project.sync(listener: (error: String?) -> Unit) {
     GradleSyncState.subscribe(this, object : GradleSyncListener {
+
         override fun syncFailed(project: Project, error: String) {
             println("Project sync failed")
             listener(error)
+        }
+
+        override fun setupStarted(p0: Project) {
+            println("Project setup started")
         }
 
         override fun syncStarted(project: Project) {
@@ -26,5 +31,7 @@ fun Project.sync(listener: (error: String?) -> Unit) {
             listener(null)
         }
     })
-    GradleProjectImporter.getInstance().requestProjectSync(this, null)
+
+    val request = GradleSyncInvoker.Request().setUseCachedGradleModels(false)
+    GradleSyncInvoker.getInstance().requestProjectSync(this, request, null)
 }
